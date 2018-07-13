fe3cbf6bde779cd4986a050959eef6fd273ea6ac
Celther <mwfister@gmail.com>
2018-07-13 16:17:40 +1000

    Basic Reducer, Dispatch method for Store
    Created a temporary visualisation of what the actions might look like.
    
    The first implementation of a reducer and dispatch have been created.

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
+// What the actions might look like
+{
+  type: 'ADD_TODO',
+  todo: {
+    id: 0,
+    name: 'Learn Redux',
+    complete: false,
+  }
+}
 
-function createStore () {
+{
+  type: 'REMOVE_TODO',
+  id: 0,
+}
+
+{
+  type: 'TOGGLE_TODO',
+  id: 0,
+}
+
+function todos (state = [], action) {
+  if (action.type === 'ADD_TODO') {
+    return state.concat([action.todo])
+  }
+
+  return state
+}
+
+function createStore (reducer) {
   // The store should have four parts
   // 1. The state - Private/Not public facing
   // 2. Get the state.
@@ -18,8 +45,14 @@ function createStore () {
     }
   }
 
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach((listener) => listener())
+  }
+
   return {
     getState,
     subscribe,
+    dispatch,
   }
 }
